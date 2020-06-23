c06ce7a76aa9d3fafef40ee5f619beb8a9e7299b
Dylan Weber <dylanweber214@gmail.com>
2020-06-23 22:37:35 +0000

    Removed the random quote generator because I felt it didn't fit my page.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,25 +11,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-/**
- * Adds a random cartoon quote to the page.
- */
-function addRandomCartoonQuote() {
-    const quotes = [
-        {quoteText: 'If every porkchop were perfect, we wouldn\'t have hot dogs', author: 'Greg Universe'},
-        {quoteText: 'Bacon pancakes, makin\' bacon pancakes.\n' +
-        'Take some bacon and I\'ll put it in a pancake.\n' +
-        'Bacon pancakes that\'s what it\'s gonna make,\n' +
-        'Bacon pancaaake!',
-        author: 'Jake the Dog'},
-        {quoteText: 'Now the pig goes wherever he can shine the light of knowledge into the darkness of ignorance.', author: 'Waddles'}
-    ]
-  
-    const quote = quotes[Math.floor(Math.random() * quotes.length)];
-
-    const formattedQuoteText = `${quote.quoteText}\n- ${quote.author}`
-
-    const quoteContainer = document.getElementById('quote-container');
-    quoteContainer.innerText = formattedQuoteText;
-}
